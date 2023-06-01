b80df9276847840ce19814cc6633bdd0e57be87f
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-06-01 15:49:53 +0200

    Add mul, div and mod operators reworked lexer

--- include/parser.h
+++ include/parser.h
@@ -13,12 +13,16 @@ typedef enum NodeType {
     NODE_OPERATION_BEGIN,
     NODE_ADD,
     NODE_SUB,
+    NODE_MUL,
+    NODE_DIV,
+    NODE_MOD,
     NODE_OPERATION_END,
 } NodeType;
 
 typedef struct Node Node;
 struct Node {
     NodeType type;
+    Token *token;
     union {
         int64_t integer;
         struct {
@@ -28,14 +32,25 @@ struct Node {
     };
 };
 
-Node *node_new(NodeType type);
+Node *node_new(NodeType type, Token *token);
 
-Node *node_new_operation(NodeType type, Node *lhs, Node *rhs);
+Node *node_new_operation(NodeType type, Token *token, Node *lhs, Node *rhs);
 
 void node_dump(FILE *f, Node *node);
 
 // Parser
-Node *parser_add(Lexer *lexer);
-Node *parser_primary(Lexer *lexer);
+typedef struct Parser {
+    Token *tokens;
+    size_t tokens_size;
+    size_t position;
+} Parser;
+
+Node *parser(Token *tokens, size_t tokens_size);
+
+void parser_eat(Parser *parser, TokenType type);
+
+Node *parser_add(Parser *parser);
+Node *parser_mul(Parser *parser);
+Node *parser_primary(Parser *parser);
 
 #endif
--- src/codegen.c
+++ src/codegen.c
@@ -20,6 +20,7 @@ void codegen(Arch arch, void *code, Node *node) {
 
 // x86_64
 #define x86_64_rax 0
+#define x86_64_rcx 1
 #define x86_64_rdx 2
 
 #define x86_64_inst1(codegen, a) *(codegen)->byte++ = a
@@ -28,12 +29,16 @@ void codegen(Arch arch, void *code, Node *node) {
         x86_64_inst1(codegen, a);   \
         *(codegen)->byte++ = b;     \
     }
-#define x86_64_inst3(codegen, a, b, c) {\
-    x86_64_inst2(codegen, a, b);       \
-    *(codegen)->byte++ = c; }
-#define x86_64_inst4(codegen, a, b, c, d) { \
-    x86_64_inst3(codegen, a, b, c);       \
-    *(codegen)->byte++ = d; }
+#define x86_64_inst3(codegen, a, b, c) \
+    {                                  \
+        x86_64_inst2(codegen, a, b);   \
+        *(codegen)->byte++ = c;        \
+    }
+#define x86_64_inst4(codegen, a, b, c, d) \
+    {                                     \
+        x86_64_inst3(codegen, a, b, c);   \
+        *(codegen)->byte++ = d;           \
+    }
 
 void codegen_node_x86_64(Codegen *c, Node *node) {
     if (node->type == NODE_INTEGER) {
@@ -47,16 +52,31 @@ void codegen_node_x86_64(Codegen *c, Node *node) {
         x86_64_inst1(c, 0x50 | (x86_64_rax & 7));  // push rax
 
         codegen_node_x86_64(c, node->lhs);
-        x86_64_inst1(c, 0x58 | (x86_64_rdx & 7));  // pop rdx
+        if (node->type == NODE_DIV || node->type == NODE_MOD) {
+            x86_64_inst1(c, 0x58 | (x86_64_rcx & 7));  // pop rcx
+        } else {
+            x86_64_inst1(c, 0x58 | (x86_64_rdx & 7));  // pop rdx
+        }
 
-        if (node->type == NODE_ADD) x86_64_inst3(c, 0x48, 0x01, 0xd0);  // add rax, rdx
-        if (node->type == NODE_SUB) x86_64_inst3(c, 0x48, 0x29, 0xd0);  // sub rax, rdx}
+        if (node->type == NODE_ADD) x86_64_inst3(c, 0x48, 0x01, 0xd0);        // add rax, rdx
+        if (node->type == NODE_SUB) x86_64_inst3(c, 0x48, 0x29, 0xd0);        // sub rax, rdx
+        if (node->type == NODE_MUL) x86_64_inst4(c, 0x48, 0x0f, 0xaf, 0xc2);  // imul rax, rdx
+        if (node->type == NODE_DIV) {
+            x86_64_inst2(c, 0x48, 0x99);        // cqo
+            x86_64_inst3(c, 0x48, 0xf7, 0xf9);  // idiv rcx
+        }
+        if (node->type == NODE_MOD) {
+            x86_64_inst2(c, 0x48, 0x99);        // cqo
+            x86_64_inst3(c, 0x48, 0xf7, 0xf9);  // idiv rcx
+            x86_64_inst3(c, 0x48, 0x89, 0xd0);  // mov rax, rdx
+        }
     }
 }
 
 // arm64
 #define arm64_x0 0
 #define arm64_x1 1
+#define arm64_x2 2
 
 #define arm64_inst(codegen, inst) *(codegen)->word++ = inst
 
@@ -74,5 +94,11 @@ void codegen_node_arm64(Codegen *c, Node *node) {
 
         if (node->type == NODE_ADD) arm64_inst(c, 0x8B010000);  // add x0, x0, x1
         if (node->type == NODE_SUB) arm64_inst(c, 0xCB010000);  // sub x0, x0, x1
+        if (node->type == NODE_MUL) arm64_inst(c, 0x9B017C00);  // mul x0, x0, x1
+        if (node->type == NODE_DIV) arm64_inst(c, 0x9AC10C00);  // sdiv x0, x0, x1
+        if (node->type == NODE_MOD) {
+            arm64_inst(c, 0x9AC10802);  // udiv x2, x0, x1
+            arm64_inst(c, 0x9B018040);  // msub x0, x2, x1, x0
+        }
     }
 }
