5ec78d7e7ee84f684e43b81980f37c9c914872b3
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-06-06 16:29:28 +0200

    Add string literal escape tests

--- build.sh
+++ build.sh
@@ -199,17 +199,45 @@ if [ "$1" = "test" ]; then
     assert 0 'int main() { return "abc"[3]; }'
     assert 4 'int main() { return sizeof("abc"); }'
 
-    # assert 98 'char *name;
-    # unsigned int hash(char *key) {
-    #     unsigned int hash = 2166136261;
-    #     while (*key) {
-    #         hash ^= *key;
-    #         key += 4;
-    #         hash *= 16777619;
+    assert 7 'int main() { return "\\a"[0]; }'
+    assert 8 'int main() { return "\\b"[0]; }'
+    assert 9 'int main() { return "\\t"[0]; }'
+    assert 10 'int main() { return "\\n"[0]; }'
+    assert 11 'int main() { return "\\v"[0]; }'
+    assert 12 'int main() { return "\\f"[0]; }'
+    assert 13 'int main() { return "\\r"[0]; }'
+    assert 27 'int main() { return "\\e"[0]; }'
+    assert 106 'int main() { return "\\j"[0]; }'
+    assert 107 'int main() { return "\\k"[0]; }'
+    assert 108 'int main() { return "\\l"[0]; }'
+    assert 7 'int main() { return "\\ax\\ny"[0]; }'
+    assert 120 'int main() { return "\\ax\\ny"[1]; }'
+    assert 10 'int main() { return "\\ax\\ny"[2]; }'
+    assert 121 'int main() { return "\\ax\\ny"[3]; }'
+    assert 0 'int main() { return "\\0"[0]; }'
+    assert 16 'int main() { return "\\20"[0]; }'
+    assert 65 'int main() { return "\\101"[0]; }'
+    assert 104 'int main() { return "\\1500"[0]; }'
+    assert 0 'int main() { return "\\x00"[0]; }'
+    assert 119 'int main() { return "\\x77"[0]; }'
+    assert 165 'int main() { return "\\xA5"[0]; }'
+    assert 255 'int main() { return "\\x00ff"[0]; }'
+
+    # assert 98 '
+    #     unsigned int hash(char *key) {
+    #         unsigned int hash = 2166136261;
+    #         while (*key) {
+    #             hash ^= *key;
+    #             key += 4;
+    #             hash *= 16777619;
+    #         }
+    #         return hash;
     #     }
-    #     return hash;
-    # }
-    # int main() { name = "Bastiaan"; return hash(name); }'
+    #     int main() {
+    #         char *name = "Bastiaan";
+    #         return hash(name);
+    #     }
+    # '
 
     echo "[OK] All tests pass"
 fi
